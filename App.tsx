c5c400afbb95d8ce0951856117d6d9cd198bcee4
Derek Prieur <DPrieur123@gmail.com>
2024-04-01 09:18:28 -0400

    update

--- App.tsx
+++ App.tsx
@@ -2,13 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView } from "react-native";
 import CardList from "./src/components/CardList";
 import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <CardList />
-      <StatusBar style="light" />
-    </SafeAreaView>
+    <GestureHandlerRootView
+      style={{
+        flex: 1,
+      }}
+    >
+      <SafeAreaView style={styles.container}>
+        <CardList />
+        <StatusBar style="light" />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
